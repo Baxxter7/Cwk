ebdf22c08bcb1fa6c3819bcf4b47bc9bb59c6de1
Josue Martinez <josueisaiasmartinezlopez@gmail.com>
2025-11-08 08:22:25 -0600

    Feat: Agregando implementacion de GetAllUsersAsync

--- Cwk.Infrastructure/Repositories/UserRepository.cs
+++ Cwk.Infrastructure/Repositories/UserRepository.cs
@@ -1,19 +1,27 @@
 using Cwk.Domain.Entities;
 using Cwk.Domain.Interfaces;
+using Cwk.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace Cwk.Infrastructure.Repositories;
 
 public class UserRepository : IUserRepository
 {
+    private readonly AppDbContext _repository;
+
+    public UserRepository(AppDbContext repository)
+    {
+        _repository = repository;
+    }
     public Task<User> GetByIdAsync(int id)
     {
         throw new NotImplementedException();
     }
 
-    public Task<IEnumerable<User>> GetAllAsync()
-    {
-        throw new NotImplementedException();
-    }
+    public async Task<IEnumerable<User>> GetAllAsync() => await _repository.Users
+            .AsNoTracking()
+            .Where(e => e.IsActive)
+            .ToListAsync();
 
     public Task<User> CreateAsync(User user)
     {
