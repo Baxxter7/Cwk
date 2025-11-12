4149bad5284c6f3c5a17ac357a462dcf3cb3e494
Josue Martinez <josueisaiasmartinezlopez@gmail.com>
2025-11-11 21:41:53 -0600

    Feat: Configuracion inicial de Blazor

--- Cwk.Web/Program.cs
+++ Cwk.Web/Program.cs
@@ -1,11 +1,14 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Cwk.Web;
+using MudBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+var url = "http://localhost:5142";
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(url) });
 
+builder.Services.AddMudServices();
 await builder.Build().RunAsync();
