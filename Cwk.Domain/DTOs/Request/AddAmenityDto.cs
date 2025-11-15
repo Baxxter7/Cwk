48a9273f8e056f9c20362e6b0e49e5b3fedea886
Josue Martinez <josueisaiasmartinezlopez@gmail.com>
2025-11-14 19:36:03 -0600

    Feat: Creacion de DTOs para amenity

---
+++ Cwk.Domain/DTOs/Request/AddAmenityDto.cs
@@ -0,0 +1,7 @@
+namespace Cwk.Domain.DTOs.Request;
+
+public class AddAmenityDto
+{
+    public string Name { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+}
