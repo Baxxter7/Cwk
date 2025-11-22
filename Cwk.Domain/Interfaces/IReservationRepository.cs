942529ae3c5617c3de2b50308bd4507d583c3eed
Josue Martinez <josueisaiasmartinezlopez@gmail.com>
2025-11-22 14:13:01 -0600

    feat: agregando IReservationRepository

---
+++ Cwk.Domain/Interfaces/IReservationRepository.cs
@@ -0,0 +1,14 @@
+﻿using Cwk.Domain.Entities;
+
+namespace Cwk.Domain.Interfaces;
+
+public interface IReservationRepository
+{
+    Task<Reservation> GetByIdAsync(int id);
+
+    Task<List<Reservation>> GetAvailablesAsync(int spaceId, DateTime startTime, DateTime endTime);
+    Task<List<Reservation>> GetAllAsync();
+    Task<Reservation> AddAsync(Reservation reservation);
+    Task<Reservation> UpdateAsync(Reservation reservation);
+    Task<List<Reservation>> GetBySpaceIdAsync(int spaceId);
+}
