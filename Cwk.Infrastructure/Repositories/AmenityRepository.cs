5b5888fdc5c9246a49b413a20a793a39ecbe3679
Josue Martinez <josueisaiasmartinezlopez@gmail.com>
2025-11-21 21:52:55 -0600

    feat: Agregando implementacion de  GetAmenitesBySpaceId

--- Cwk.Infrastructure/Repositories/AmenityRepository.cs
+++ Cwk.Infrastructure/Repositories/AmenityRepository.cs
@@ -15,20 +15,28 @@ public class AmenityRepository : IAmenityRepository
     }
 
     public async Task<Amenity?> GetByAmenityId(int amenityId) => await _context.Amenities.FindAsync(amenityId);
-    
+
 
     public async Task<Amenity> AddAmenityAsync(Amenity amenity)
     {
-         await _context.AddAsync(amenity);
-         await _context.SaveChangesAsync();
-         return amenity;
+        await _context.AddAsync(amenity);
+        await _context.SaveChangesAsync();
+        return amenity;
     }
 
     public async Task<List<Amenity>> GetAllAmenitiesAsync() => await _context.Amenities.ToListAsync();
-    
+
     public async Task UpdateAmenityAsync(Amenity amenity)
     {
         _context.Amenities.Update(amenity);
         await _context.SaveChangesAsync();
+    }
+
+    public async Task<List<Amenity>> GetAmenitesBySpaceIdAsync(int spaceId)
+    {
+        return await _context.SpaceAmenities
+            .Where(sa => sa.SpaceId == spaceId)
+            .Select(sa => sa.Amenity)
+            .ToListAsync();
     }
 }
