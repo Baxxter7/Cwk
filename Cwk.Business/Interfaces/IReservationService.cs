99360d044124ceb6a02aecba3d414dcbdb89813d
Josue Martinez <josueisaiasmartinezlopez@gmail.com>
2025-11-23 23:55:08 -0600

    feat: agregando IReservationService

---
+++ Cwk.Business/Interfaces/IReservationService.cs
@@ -0,0 +1,16 @@
+﻿using Cwk.Domain.DTOs.Request;
+using Cwk.Domain.DTOs.Response;
+
+namespace Cwk.Business.Interfaces;
+
+public interface IReservationService
+{
+    Task<ReservationResponseDto> GetReservationsAsync(ReservationQueryDto query);
+    Task<ReservationDetailsDto?> GetReservationByIdAsync(int id);
+    Task<ReservationDetailsDto> CreateReservationAsync(CreateReservationRequestDto request);
+    Task<ReservationDetailsDto?> UpdateReservationAsync(UpdateReservationRequestDto request);
+    Task<bool> DeleteReservationAsync(int id);
+    Task<SpaceAvailabilityDto?> CheckSpaceAvilabilityAsync(int spaceId, DateTime startTime, DateTime endTime);
+    Task<bool> ConfirmReservationAsync(int reservationId);
+    Task<bool> CancelReservationAsync(int reservationId);
+}
