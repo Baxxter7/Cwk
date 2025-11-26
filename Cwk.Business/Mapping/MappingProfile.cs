20d388cc2163490b38ab7709d1b597a11e38ddd7
Josue Martinez <josueisaiasmartinezlopez@gmail.com>
2025-11-25 22:40:05 -0600

    feat: agregando mapeo de reservations en el mapping profile

--- Cwk.Business/Mapping/MappingProfile.cs
+++ Cwk.Business/Mapping/MappingProfile.cs
@@ -10,12 +10,18 @@ public class MappingProfile : Profile
     public MappingProfile()
     {
         CreateMap<User, UserResponseDto>();
+        
         CreateMap<Amenity, AmenityResponseDto>();
         CreateMap<AddAmenityDto, Amenity>();
         CreateMap<UpdateAmenityDto, Amenity>();
+        
         CreateMap<Space, SpaceDetailsDto>();
         CreateMap<AddSpaceDto, Space>();
         CreateMap<EditSpaceDto, Space>()
-              .ForMember(dest => dest.ImageUrl, opt => opt.Ignore()); 
+              .ForMember(dest => dest.ImageUrl, opt => opt.Ignore());
+
+        CreateMap<Reservation, ReservationDetailsDto>();
+        CreateMap<CreateReservationRequestDto, Reservation>();
+        CreateMap<UpdateReservationRequestDto, Reservation>();
     }
 }
