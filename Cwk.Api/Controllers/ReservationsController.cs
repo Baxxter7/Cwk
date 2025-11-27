aea0ca9718f2c87adafa704ef4a03dbc4c9fbfaf
Josue Martinez <josueisaiasmartinezlopez@gmail.com>
2025-11-26 20:54:09 -0600

    feat: agregando ReservationsController con la implementacion de CreateReservationAsync y CheckAvailability

---
+++ Cwk.Api/Controllers/ReservationsController.cs
@@ -0,0 +1,44 @@
+﻿using Cwk.Business.Interfaces;
+using Cwk.Domain.DTOs.Request;
+using Cwk.Domain.DTOs.Response;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cwk.Api.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ReservationsController : ControllerBase
+{
+    private readonly IReservationService _reservationService;
+
+    public ReservationsController(IReservationService reservationService)
+    {
+        _reservationService = reservationService;
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<ReservationDetailsDto>> CreateReservation([FromBody] CreateReservationRequestDto request)
+    {
+        try
+        {
+            var reservation = await _reservationService.CreateReservationAsync(request);
+            return Ok(reservation);
+        }
+        catch (InvalidOperationException ex)
+        {
+            return BadRequest(ex.Message);
+
+        }
+        catch (ArgumentException ex) { 
+            return NotFound(ex.Message);
+        }
+    }
+
+    [HttpGet("availability/{spaceId}")]
+    public async Task<ActionResult<SpaceAvailabilityDto>> CheckAvailability(int spaceId, [FromQuery] DateTime startTime, [FromQuery] DateTime endTime)
+    {
+        var availability = await _reservationService.CheckSpaceAvailabilityAsync(spaceId, startTime, endTime);
+        return Ok(availability);
+    }
+
+}
