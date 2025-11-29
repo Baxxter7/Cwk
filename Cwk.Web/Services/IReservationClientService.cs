e06f4b96ec03e527298179fc0a319b4799a90770
Josue Martinez <josueisaiasmartinezlopez@gmail.com>
2025-11-28 23:00:16 -0600

    feat: definicion e implementacion de ReservationClientService

---
+++ Cwk.Web/Services/IReservationClientService.cs
@@ -0,0 +1,14 @@
+﻿using Cwk.Domain.DTOs.Request;
+using Cwk.Domain.DTOs.Response;
+
+namespace Cwk.Web.Services;
+
+public interface IReservationClientService
+{
+    Task<ReservationDetailsDto> CreateReservationAsync(CreateReservationRequestDto request);
+    Task<ReservationResponseDto> GetReservationsAsync(ReservationQueryDto query);
+    Task<SpaceAvailabilityDto> CheckSpaceAvailabilityAsync(int spaceId, DateTime startTime, DateTime endTime);
+    Task<ReservationResponseDto> GetUserReservationsAsync(int userId);
+    Task<bool> ConfirmReservationAsync(int reservationId);
+    Task<bool> CancelReservationAsync(int reservationId);
+}
